0cf946f557d88475e65718b4bf0266639e3357c0
Chun-Wei Chang <chunwei.chang.cwc@gmail.com>
2025-10-16 01:17:49 +0900

    feat: layout page use MarkerDataProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,29 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { MarkerDataProvider } from "@/providers/MarkerDataProvider";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
+    <ThemeProvider value={DefaultTheme}>
+      <MarkerDataProvider>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "Home",
+            }}
+          />
+          <Stack.Screen
+            name="explore"
+            options={{
+              title: "Explore",
+            }}
+          />
+        </Stack>
+        <StatusBar style="auto" />
+      </MarkerDataProvider>
     </ThemeProvider>
   );
 }
